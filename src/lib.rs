76493be01132a7c507fe6ac906efd296dcefc68b
Dennis Möhlmann <dmo479@gmail.com>
2017-01-08 16:25:50 +0100

    extracted from radiant-rs

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+mod avec;
+
+pub use avec::{AVec, AVecReadGuard, AVecMapGuard};
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+extern crate avec;
+use avec::AVec;
+
+fn main() {
+
+    let x = AVec::<u32>::new(32);
+
+}
